46c7073acef35ffa764e9cd3c6cfc3579184a9c9
kitkat <kifah.andary@gmail.com>
2019-12-05 20:18:43 +0200

    added spinners and recycler view

---
+++ app/src/main/java/com/example/lazycashier/CurrencyAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.lazycashier
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class CurrencyAdapter(
+    private val data: ArrayList<Double?>,
+    private val flags: IntArray
+) :
+    RecyclerView.Adapter<CurrencyAdapter.CurrencyViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        i: Int
+    ): CurrencyViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view: View = inflater.inflate(R.layout.currency_text_recycler, parent, false)
+        return CurrencyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(
+        viewHolder: CurrencyViewHolder,
+        i: Int
+    ) {
+        val title = data[i]
+        viewHolder.txt.text = title.toString()
+        viewHolder.m.setImageResource(flags[i])
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    inner class CurrencyViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+        var m: ImageView
+        var txt: TextView
+
+        init {
+            m = itemView.findViewById(R.id.curimg)
+            txt = itemView.findViewById(R.id.curtxt)
+        }
+    }
+
+}
